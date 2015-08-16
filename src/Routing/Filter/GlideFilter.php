72102e6f2f9aec1a08f831ca7163a39f847d9d2f
ADmad <admad.coder@gmail.com>
2015-08-17 01:13:42 +0530

    Fix CS error

--- src/Routing/Filter/GlideFilter.php
+++ src/Routing/Filter/GlideFilter.php
@@ -12,20 +12,20 @@ use League\Glide\Signatures\SignatureFactory;
 class GlideFilter extends DispatcherFilter
 {
 
-	/**
-	 * Callback for Routing.beforeDispatch event.
-	 *
-	 * @param \Cake\Event\Event $event The event instance.
-	 * @return \Cake\Network\Response Response instance.
-	 */
+    /**
+     * Callback for Routing.beforeDispatch event.
+     *
+     * @param \Cake\Event\Event $event The event instance.
+     * @return \Cake\Network\Response Response instance.
+     */
     public function beforeDispatch(Event $event)
     {
         $request = $event->data['request'];
         $path = urldecode('/' . $request->url);
 
         if (Configure::read('Glide.secureUrls')) {
-	        SignatureFactory::create(Security::salt())
-	            ->validateRequest($path, $request->query);
+            SignatureFactory::create(Security::salt())
+                ->validateRequest($path, $request->query);
         }
 
         $server = ServerFactory::create(Configure::read('Glide.serverConfig'));
@@ -33,10 +33,10 @@ class GlideFilter extends DispatcherFilter
 
         $headers = Hash::filter((array)Configure::read('Glide.headers'));
         if (!empty($headers['Expires']) && $headers['Expires'] === true) {
-        	$headers['Expires'] = date_create('+1 years')->format('D, d M Y H:i:s') . ' GMT';
+            $headers['Expires'] = date_create('+1 years')->format('D, d M Y H:i:s') . ' GMT';
         }
         foreach ($headers as $key => $value) {
-        	$response->header($key, $value);
+            $response->header($key, $value);
         }
 
         return $response;
